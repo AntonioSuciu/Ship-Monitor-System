1ca3daa1550a9d2431141008af9e1c557b906103
antoniosuciu <suciu.antonio.tudor@gmail.com>
2022-04-01 11:34:52 +0300

    Added Backend
    A Windows Forms Application (C#), to allow for data visualization.

---
+++ Ships/Ships/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Ships
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewVoyage = new System.Windows.Forms.DataGridView();
+            this.textBoxShipId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonAvgDays = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVoyage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewVoyage
+            // 
+            this.dataGridViewVoyage.AllowUserToAddRows = false;
+            this.dataGridViewVoyage.AllowUserToDeleteRows = false;
+            this.dataGridViewVoyage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewVoyage.Location = new System.Drawing.Point(12, 54);
+            this.dataGridViewVoyage.Name = "dataGridViewVoyage";
+            this.dataGridViewVoyage.RowHeadersWidth = 62;
+            this.dataGridViewVoyage.RowTemplate.Height = 28;
+            this.dataGridViewVoyage.Size = new System.Drawing.Size(1323, 271);
+            this.dataGridViewVoyage.TabIndex = 0;
+            this.dataGridViewVoyage.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBoxShipId
+            // 
+            this.textBoxShipId.Location = new System.Drawing.Point(600, 389);
+            this.textBoxShipId.Name = "textBoxShipId";
+            this.textBoxShipId.Size = new System.Drawing.Size(113, 26);
+            this.textBoxShipId.TabIndex = 1;
+            this.textBoxShipId.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(596, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Insert a ship ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonAvgDays
+            // 
+            this.buttonAvgDays.Location = new System.Drawing.Point(580, 421);
+            this.buttonAvgDays.Name = "buttonAvgDays";
+            this.buttonAvgDays.Size = new System.Drawing.Size(151, 58);
+            this.buttonAvgDays.TabIndex = 4;
+            this.buttonAvgDays.Text = "Get the average number of days";
+            this.buttonAvgDays.UseVisualStyleBackColor = true;
+            this.buttonAvgDays.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1347, 581);
+            this.Controls.Add(this.buttonAvgDays);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxShipId);
+            this.Controls.Add(this.dataGridViewVoyage);
+            this.Name = "Form1";
+            this.Text = "Ship Management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVoyage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewVoyage;
+        private System.Windows.Forms.TextBox textBoxShipId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAvgDays;
+    }
+}
+
---
+++ Ships/Ships/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ships
+{
+    using System.Data;
+    using System.Data.SqlClient;
+
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection;
+        SqlDataAdapter dataAdapterVoyage;
+
+        SqlCommandBuilder cb;
+
+        DataSet dataSet = new DataSet();
+
+        BindingSource bindingSourceVoyage= new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection("Data Source = DESKTOP-B3OL4T0; Initial Catalog = ShipsMonitoringSystem; Integrated Security=True");
+
+            dataAdapterVoyage = new SqlDataAdapter("Select * from Voyage", sqlConnection);
+
+            cb = new SqlCommandBuilder(dataAdapterVoyage);
+
+            dataAdapterVoyage.Fill(dataSet, "Voyage");
+
+            bindingSourceVoyage.DataSource = dataSet;
+            bindingSourceVoyage.DataMember = "Voyage";
+
+            dataGridViewVoyage.DataSource = bindingSourceVoyage;
+
+            dataGridViewVoyage.ClearSelection();
+            dataGridViewVoyage.Rows[bindingSourceVoyage.Position].Selected = true;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // create a command object identifying the stored procedure
+            SqlCommand command = new SqlCommand("ship_avg_voyage_days", sqlConnection);
+
+            DataTable dt = new DataTable();
+
+            try
+            {            // set the command object so it knows to execute a stored procedure
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.Add("@voyage_ship_id", SqlDbType.Int).Value = textBoxShipId.Text;
+
+                sqlConnection.Open();
+                dataAdapterVoyage.SelectCommand = command;
+                dataAdapterVoyage.Fill(dt);
+                dataGridViewVoyage.DataSource = dt;
+                sqlConnection.Close();
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.GetBaseException().ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        
+        }
+    }
+}
